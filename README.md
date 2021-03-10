b1631863e5006a527924a4aff95c02f6ff490987
spencerjbeckwith <spencerjbeckwith@gmail.com>
2021-03-09 18:10:23 -0700

    recursive-resize

--- README.md
+++ README.md
@@ -1,7 +1,34 @@
 # recursive-resize
 
-This simple command-line tool takes a directory and will resize all images within that directory to a specified file size outputting them in a the same folder structure in an output directory. It can also generate thumbnails of an even smaller size.
+This simple command-line tool resizes all images in a directory to a specified width, and outputs them in an output directory. It can also generate thumbnails of an even smaller size, all prefixed with "tn-".
+
+This is meant to be used to quickly shrink a large amount of heavy files, to save on loading times and network usage. Images will maintain their aspect ratio. 
 
 To install, use `npm install -g recursive-resize`
 
-To run, you use `recursive-resize` (or if you're like me and that's too many letters, you can use `recres` instead.)
+To run, you use `recursive-resize` (or if that's too many letters, you can use `recres` instead.)
+
+===
+
+## CLI Options
+
+* `-i, --input`: Defines the input directory. Every .png, .jpg, and .jpeg file within the directory will be resized. Can
+  * If no input directory is defined, the current directory will be used. **If you do this, make sure your output directory is NOT inside the working directory!** You should always define both input and output.
+* `-o, --output`: Defines the output directory. The same file structure from the input directory will be created. Any existing files with the same filenames will be overwritten. 
+  * If no output directory is defined, the current directory will be used. **If you do this, make sure you aren't outputting into your input directory!** You should always define both input and output.
+* `-w, --width`: The new width of every image. Because aspect ratio is meant to be maintained there is no height option.
+* `-t, --thumbnail`: The new width for each thumbnail. If not defined, no thumbnails will be generated. Thumbnail files are prefixed with "tn-" before the image's regular filename.
+* `-d, --debug`: Print additional output.
+* `-h, --help`: Shows these options.
+
+If you want to overwrite your input directory with your output, you can use `./` as both the `-i` and `-o` arguments. You probably *shouldn't* do this though. And you also probably *shouldn't* use an output directory as an input directory.
+
+===
+
+## Examples
+
+`recres -i fullsize-images -o public/images -w 2000 -t 256` Resizes all images from the directory fullsize-images and places it into the public/images directory at 2000px wide, along with thumbnails for each image that are 256px wide.
+
+`recres -o ../newimages -w 6000` Resizes all images from current working directory newimages, sizes them up to 6000px across, and puts them into an outer folder.
+
+`recres -i /home/user/images -w 25 -t 200 -d` Resizes all images from absolute directory /home/user/images to be 25px wide, with their thumbnails 200px wide. I dunno why you'd do this... but no judgement here. This also prints progress and extra information.
--- bin/index.js
+++ bin/index.js
@@ -1,2 +1,155 @@
 #!/usr/bin/env node
 
+import fs from 'fs';
+import path from 'path';
+
+import { program } from 'commander/esm.mjs';
+import sharp from 'sharp';
+
+program
+    .usage('recursive-resize|recres [options]')
+    .option('-i, --input <directory>','input directory to recursively resize')
+    .option('-o, --output <directory>','output directory to place resized images/thumbnails')
+    .requiredOption('-w, --width <pixels>','the new width for each image')
+    .option('-t, --thumbnail <pixels>','if defined, the new width for each thumbnail')
+    .option('-d, --debug','show extra output');
+program.parse(process.argv);
+const options = program.opts();
+
+// Validate the options
+if (!options.input && !options.output) {
+    console.log('You must specify either an input or an output directory. For help, use recres -h');
+    process.exit(0);
+} else {
+    if (!options.input) {
+        options.input = path.join(process.cwd(),'/');
+        console.log(`No input directory specified, using ${options.input}`);
+    }
+    if (!options.output) {
+        options.output = path.join(process.cwd(),'/');
+        console.log(`No output directory specifeid, using ${options.input}`);
+    }
+}
+
+// Don't allow invalid numbers/entries
+options.width = Number(options.width);
+if (options.width <= 0) {
+    console.log('Width must be greater than 0!');
+    process.exit(0);
+}
+if (options.thumbnail) {
+    options.thumbnail = Number(options.thumbnail);
+    if (options.thumbnail <= 0) {
+        console.log('Thumbnail width must be greater than 0!');
+        process.exit(0);
+    }
+}
+
+// Log that we're starting
+options.input = path.resolve(options.input);
+options.output = path.resolve(options.output);
+console.log(`Recursively resizing all images from ${options.input} to ${options.output}, changing with to ${options.width}px.`);
+if (options.thumbnail) {
+    console.log(`Generating thumbnails with width of ${options.thumbnail}px.`);
+}
+
+// Crawl input directory
+let totalImages = 0;
+const imageArray = [];
+function crawlDirectory(directory,branch) {
+    let directoryTotal = 0;
+    const dir = fs.opendirSync(directory);
+
+    // Check if this directory exists in output, and if it doesn't, create it.
+    if (!fs.existsSync(path.resolve(options.output,branch))) {
+        fs.mkdirSync(path.resolve(options.output,branch));
+    }
+
+    let item = dir.readSync();
+    console.group();
+    while (item) {
+        if (item.isDirectory()) {
+            // Call recursively
+            crawlDirectory(path.resolve(directory,item.name),branch+'/'+item.name);
+        }
+
+        if (item.isFile()) {
+            // See if it matches our descriptions
+            if (/.+((\.png)|(\.jpg)|(\.jpeg))/i.test(item.name)) {
+                // Found an image
+                directoryTotal++;
+                totalImages++;
+                imageArray.push({
+                    fullsize: branch+'/'+item.name,
+                    thumbnail: (options.thumbnail ? branch+'/tn-'+item.name : '')
+                })
+                if (options.debug) {
+                    console.log(branch+'/'+item.name);
+                }
+            }
+        }
+
+        item = dir.readSync();
+    }
+    console.groupEnd();
+    dir.closeSync();
+    if (options.debug) {
+        console.log(`Found ${directoryTotal} images in ${directory}`);
+    }
+}
+
+// Actually do the crawling
+let time = Date.now();
+crawlDirectory(options.input,'.');
+console.log(`Found a total of ${totalImages} images in ${Date.now()-time}ms.`);
+console.log(`Resizing images...`);
+
+// Set up for our loop
+let progress = 0;
+if (options.thumbnail) {
+    // Twice as many images...
+    totalImages *= 2;
+}
+time = Date.now();
+
+// Ah, the big boi loop
+for (let i = 0; i < imageArray.length; i++) {
+    // Resize and rewrite each file
+    sharp(path.resolve(options.input,imageArray[i].fullsize))
+    .resize(options.width)
+    .toFile(path.resolve(options.output,imageArray[i].fullsize))
+    .then((value) => {
+        progressed(imageArray[i].fullsize);
+    })
+    .catch((err) => {
+        console.log(`Problem outputting ${imageArray[i].fullsize}!`);
+        console.error(err);
+        process.exit(1);
+    });
+    // Thumbnail?
+    if (options.thumbnail) {
+        sharp(path.resolve(options.input,imageArray[i].fullsize))
+        .resize(options.thumbnail)
+        .toFile(path.resolve(options.output,imageArray[i].thumbnail))
+        .then((value) => {
+            progressed(imageArray[i].thumbnail);
+        })
+        .catch((err) => {
+            console.log(`Problem outputting thumbnail ${imageArray[i].thumbnail}!`);
+            console.error(err);
+            process.exit(1);
+        });
+    }
+}
+
+// Call when each image is loaded
+function progressed(filename) {
+    progress++;
+    if (options.debug) {
+        console.log(`${Math.trunc(progress/totalImages*100)}% - ${filename}`);
+    }
+    if (progress >= totalImages) {
+        console.log(`Done! Output ${totalImages} images in ${Date.now()-time}ms.`);
+        process.exit(0);
+    }
+}
